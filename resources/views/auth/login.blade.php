aa14fc550eec16158dbf43b3904a2e4bed102eb3
christopher <christojhg@gmail.com>
2021-10-20 17:53:13 -0500

    auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
+<div class="container ">
+    <div class="row justify-content-center div_center ">
         <div class="col-md-8">
-            <div class="card">
+            <div class="overlay">
+                <div class="card ">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
@@ -23,24 +24,13 @@
                                     </span>
                                 @enderror
                             </div>
+                            <input id="password" type="password" hidden="" value="1" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                         </div>
-                        <input id="password" type="password" hidden="" value="1" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                        
-                       {{--  <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                            {{-- </div>
                         </div> --}}
 
-                        <div class="form-group row">
+                        {{-- <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -50,7 +40,7 @@
                                     </label>
                                 </div>
                             </div>
-                        </div>
+                        </div> --}}
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
@@ -58,17 +48,38 @@
                                     {{ __('Login') }}
                                 </button>
 
-                                @if (Route::has('password.request'))
+                                {{-- @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
-                                @endif
+                                @endif --}}
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
+            </div>
         </div>
     </div>
 </div>
+{{-- <style type="text/css">
+    .card{
+        /*top: 100%;*/
+        margin: 30px;
+        margin-top: 30px;
+        margin-bottom: 30px;
+    }
+    .overlay{
+        top: 100%;
+        background-color: #8cb2d969;
+        padding-top: 1px;
+        padding-bottom: 1px;
+        border-radius: 15px;
+        border: 2px  grey solid;
+        box-shadow: 10px 5px black ;
+    }
+    body{
+         background-color: #d2d98c;
+        }
+</style> --}}
 @endsection
