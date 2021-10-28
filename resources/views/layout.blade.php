498792bd19a847e236ab5f178ba6b32848c08544
Fernando <fernandomv.0102@gmail.com>
2021-10-28 18:02:07 -0500

    init

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -33,11 +33,11 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                   {{--  <ul class="navbar-nav ml-auto">
                             <li class="nav-item">
                                 <a class="nav-link" href="{{route('empresa.index')}}">Empresas</a>
                             </li>
-                    </ul>
+                    </ul> --}}
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
